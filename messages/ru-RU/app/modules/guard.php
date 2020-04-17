5f012563dc89202d12ef86434c071058b3e2cbe3
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2020-04-17 03:11:46 +0300

    Transaltions fixed

--- messages/ru-RU/app/modules/guard.php
+++ messages/ru-RU/app/modules/guard.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'It seems that you have not changed your access password for a long time. We recommend that you, periodically, change the password for access to the administrative interface of the site.' => "Похоже, что Вы давно не меняли пароль доступа. Рекомендуем Вам, переодический, менять пароль доступа к административному интерфейсу сайта.",
+    'It seems that you have not changed your access password for a long time. We recommend that you, periodically, change the password for access to the administrative interface of the site.' => "Похоже, что Вы давно не меняли пароль доступа. Рекомендуем переодически менять пароль доступа к административному интерфейсу сайта.",
 
 ];
 
